8343875b4a709911b6f153dc853e96665541c4ef
Hernanduer <epasonic@gmail.com>
2015-09-10 16:01:50 -0500

    Added settings.js and url for unowned rooms

--- extensions/say.js
+++ extensions/say.js
@@ -1,18 +1,18 @@
 module.exports = {
-    handle: 'say',
-    commands: [
-        {
-            command: 'say',
-            description: 'Administrator command to make the bot say things to a specified channel',
-            script: function(from, args, to, text) {
-                if (Bot.approved.indexOf(from) > -1) {
-                    var re = /!say\s+(#+\w+)\s+(.+)/gi;
+	handle: 'say',
+	commands: [
+		{
+			command: 'say',
+			description: 'Administrator command to make the bot say things to a specified channel',
+			script: function(from, args, to, text) {
+				if (Bot.approved.indexOf(from) > -1) {
+					var re = /!say\s+(#+\w+)\s+(.+)/gi;
 
-                    var result = re.exec(text);
+					var result = re.exec(text);
 
-                    irc.say(result[1], result[2]);
-                }
-            }
-        }
-    ]
+					irc.say(result[1], result[2]);
+				}
+			}
+		}
+	]
 };
